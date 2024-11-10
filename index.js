a9370d7a3270928769ade8e309c587462e31a37a
zakaria-statistics <zakariaelmoumnaoui96@gmail.com>
2024-11-10 22:20:33 +0100

    Container bootup order

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 const app = express();
 
-mongoose
-    .connect("mongodb://admin:1234@mongo:27017/?authSource=admin")
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () =>{
+    mongoose
+    .connect(mongoUrl)
     .then(() => console.log("succesfully connected to DB"))
-    .catch((e) => console.log("error connecting to db => ", e));
+    .catch((e) => {
+        console.log("error connecting to db => ", e);
+        setTimeout(connectWithRetry, 5000);
+    });
+}
+
+connectWithRetry();
+
+
+
 
 app.get("/", (req, res) => {
-    res.send("<h2>Hi There!!!</h2>")
+    res.send("<h2>Hi There</h2>")
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
